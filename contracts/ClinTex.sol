9b07426544f482b059ea886a3755b8f9c47abbc4
kralonur <kralonur1998@gmail.com>
2022-03-09 13:17:43 +0700

    feat(clintex): add minting cap to the contract
    and make mint function external

--- contracts/ClinTex.sol
+++ contracts/ClinTex.sol
@@ -1,15 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.6;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ClinTex is ERC20, Ownable {
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+contract ClinTex is ERC20Capped, Ownable {
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 cap
+    ) ERC20Capped(cap) ERC20(name, symbol) {}
 
-    //TODO: think about adding minting cap
-
-    function mint(address to, uint256 amount) public onlyOwner {
+    function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
     }
 }
