91315468e8ed0bac1d97e451c6157b9d1f6fba62
kralonur <kralonur1998@gmail.com>
2022-03-09 12:12:07 +0700

    build: create ClinTex contract

---
+++ contracts/ClinTex.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ClinTex is ERC20, Ownable {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+
+    //TODO: think about adding minting cap
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
