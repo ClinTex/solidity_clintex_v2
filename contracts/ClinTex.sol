20d2da8fcef17492632c23c86fca0e0b4708412d
kralonur <kralonur1998@gmail.com>
2022-03-11 17:27:53 +0700

    docs: write clintex documentation

--- contracts/ClinTex.sol
+++ contracts/ClinTex.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.8.6;
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+/**
+ * @title ERC20 contract with supply cap
+ * @dev See {ERC20Capped}
+ */
 contract ClinTex is ERC20Capped, Ownable {
     constructor(
         string memory name,
@@ -11,6 +15,7 @@ contract ClinTex is ERC20Capped, Ownable {
         uint256 cap
     ) ERC20Capped(cap) ERC20(name, symbol) {}
 
+    /// @dev See {ERC20Capped-_mint}
     function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
     }
